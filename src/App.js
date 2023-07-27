b272176f9b165fc5316c60e06eb8e164b28cef0f
dalimkumar604 <dalimkumar212582@gmail.com>
2023-07-28 04:54:34 +0600

    solved

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 import { BiMicrophoneOff, BiMicrophone } from "react-icons/bi";
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   const backendUrls = [
@@ -20,14 +21,7 @@ function App() {
   const handleConnected = () => {
     setConnected((prev) => !prev);
   };
-  const TIPS_URL =
-    "https://thyself.notion.site/Tips-for-talking-to-Thyself-3fd927c59df442718e0ad2c594a9f5f0?pvs=4";
-  const handleCalendlyClick = (e) => {
-    e.preventDefault();
-    window.Calendly.initPopupWidget({
-      url: "https://calendly.com/d/yvc-2bj-g4n/discovery-session-with-jonathan-lawrence",
-    });
-  };
+
   return (
     <div>
       <Header />
@@ -83,30 +77,7 @@ function App() {
           “continue”
         </p>
       </main>
-      <div id="footer-links">
-        <div className="footer-link-wrapper">
-          <a href={TIPS_URL} target="_blank">
-            <img src="/tips.png" alt="#" /> Help &amp; Tips
-          </a>
-        </div>
-        <div className="footer-link-wrapper">
-          <a
-            data-tally-open="w4BWdb"
-            data-tally-layout="modal"
-            data-tally-width="900"
-            data-tally-emoji-text=""
-            data-tally-emoji-animation="wave"
-            data-tally-auto-close="2000"
-          >
-            <img src="/heart.png" alt="#" /> Share Feedback
-          </a>
-        </div>
-        <div className="footer-link-wrapper">
-          <a href="#" onClick={handleCalendlyClick}>
-            <img src="/calender.png" alt="#" /> Schedule a Call
-          </a>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Footer = () => {
+  const TIPS_URL =
+    "https://thyself.notion.site/Tips-for-talking-to-Thyself-3fd927c59df442718e0ad2c594a9f5f0?pvs=4";
+  const handleCalendlyClick = (e) => {
+    e.preventDefault();
+    window.Calendly.initPopupWidget({
+      url: "https://calendly.com/d/yvc-2bj-g4n/discovery-session-with-jonathan-lawrence",
+    });
+  };
+  return (
+    <div id="footer-links">
+      <div className="footer-link-wrapper">
+        <a href={TIPS_URL} target="_blank">
+          <img src="/tips.png" alt="#" /> Help &amp; Tips
+        </a>
+      </div>
+      <div className="footer-link-wrapper">
+        <a
+          data-tally-open="w4BWdb"
+          data-tally-layout="modal"
+          data-tally-width="900"
+          data-tally-emoji-text=""
+          data-tally-emoji-animation="wave"
+          data-tally-auto-close="2000"
+        >
+          <img src="/heart.png" alt="#" /> Share Feedback
+        </a>
+      </div>
+      <div className="footer-link-wrapper">
+        <a href="#" onClick={handleCalendlyClick}>
+          <img src="/calender.png" alt="#" /> Schedule a Call
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
